0e0b534caa524ccbdcf2999a1a9afbd8074cbabe
Y-Mitsui <dyfomg@gmail.com>
2015-09-23 22:27:45 +0900

    implement adaptive epcilon

--- hamiltonian_monte_carlo.py
+++ hamiltonian_monte_carlo.py
@@ -9,35 +9,32 @@ def softmax(param):
     return numpy.exp(param)/numpy.exp(param).sum()
     
 def find_reasonable_epsilon(parameter, energy_function, energy_function_delta):
-    epsilon = 1.
+    epsilon = 1e-1
     momentum = numpy.random.normal(0., 1., len(parameter))
     hamilton = energy_function(parameter) - numpy.sum(momentum**2.0) / 2.0
     current_parameter_condinate, momentum = leapfrog(parameter, momentum, epsilon, energy_function_delta)
-    differenceHamilton = ( (numpy.sum(momentum ** 2.0) / 2.0 - energy_function(current_parameter_condinate)) ) - hamilton
-    accept_probability = min(1.,numpy.exp(differenceHamilton))
-
+    differenceHamilton = (energy_function(current_parameter_condinate) - numpy.sum(momentum ** 2.0) / 2.0)  - hamilton
+    accept_probability = numpy.exp(differenceHamilton)
     a = 2 * int( accept_probability > 0.5) - 1
-    print a
+
     while accept_probability ** a > 2 **-a :
         
-        epsilon = 2 ** a * epsilon
-        hamilton = energy_function(current_parameter_condinate) - numpy.sum(momentum**2.0) / 2.0
+        epsilon = 2. ** a * epsilon
         current_parameter_condinate, momentum = leapfrog(current_parameter_condinate, momentum, epsilon, energy_function_delta)
-        differenceHamilton = ( (numpy.sum(momentum ** 2.0) / 2.0 - energy_function(current_parameter_condinate)) ) - hamilton
-        accept_probability = min(1.,numpy.exp(differenceHamilton))
-        print accept_probability
+        differenceHamilton = (energy_function(current_parameter_condinate) - numpy.sum(momentum ** 2.0) / 2.0) - hamilton
+        accept_probability = numpy.exp(differenceHamilton)
         
-    print "find_reasonable_epsilon=", epsilon
+    print "find_reasonable_epsilon=", epsilon ** a
 
     return epsilon
 
 def leapfrog(current_parameter_condinate,momentum,step_accuracy,energy_function_delta):
-    momentum = momentum + step_accuracy * energy_function_delta(current_parameter_condinate)
+    momentum = momentum + (step_accuracy/2.) * energy_function_delta(current_parameter_condinate)
     current_parameter_condinate = current_parameter_condinate + step_accuracy * momentum
-    momentum = momentum + step_accuracy * energy_function_delta(current_parameter_condinate)
+    momentum = momentum + (step_accuracy/2.) * energy_function_delta(current_parameter_condinate)
     return current_parameter_condinate, momentum     
     
-def hamiltonianMonteCarlo(parameter, energy_function, energy_function_delta, iter=20000,bear_in=10000, iter_leapfrog=30, leapfrog_accuracy=0.001):
+def hamiltonianMonteCarlo(parameter, energy_function, energy_function_delta, iter=10000,bear_in=5000, iter_leapfrog=30, leapfrog_accuracy=0.001):
 
     target_accept = 0.8
     t0 = 10
@@ -72,13 +69,14 @@ def hamiltonianMonteCarlo(parameter, energy_function, energy_function_delta, ite
         differenceHamilton = ( (energy_function(current_parameter_condinate) - numpy.sum(momentum ** 2.0) / 2.0) ) - hamilton
 
         accept_probability = min(1.,numpy.exp(differenceHamilton))
-        
+        #print "accept_probability:{}".format(accept_probability)
         H_t = target_accept - accept_probability
         w = 1. / ( i + t0 )
         average_H = (1 - w) * average_H + w * H_t
+        #print u-(numpy.sqrt(i)/gamma)*average_H
         step_size = numpy.exp(u - (numpy.sqrt(i)/gamma)*average_H)
         
-        if i % 10 == 0:
+        if i % 100 == 0:
             print "------ %d ------"%(i)
             print "accept_probability:{}".format(accept_probability)
             print "average_H:{}".format(average_H)
@@ -94,32 +92,32 @@ def hamiltonianMonteCarlo(parameter, energy_function, energy_function_delta, ite
             print "likelyfood:%f"%(energy_function(current_parameter_condinate))
         if i > bear_in and i % 10 == 0:
             r.append(softmax(current_parameter))
+        #time.sleep(0.5)
     print numpy.average(r,axis=0)
     return current_parameter
 def bern(p):
     return numpy.random.uniform() < p    
 def callPosterior(parameter):
     #print posterior([2,2,2],parameter,0,5)
-    return posterior([200,200,200,200,200,200,200,200,200,200],parameter,0,100)
+    return posterior([20,20,20],parameter)
 def callGPosterior(parameter):
     #print gPosterior([2,2,2],parameter,0,5)
-    return gPosterior([200,200,200,200,200,200,200,200,200,200],parameter,0,100)
+    return gPosterior([20,20,20],parameter)
     
 x = T.dvector('x')
-u = T.dscalar('u')
-sigma = T.dscalar('sigma')
 nSample = T.dvector('nSample')
-normalPdfSyntax = 1. / T.sqrt(2 * 3.14159 * sigma ** 2) * T.exp(-(x-u) ** 2 / (2 * sigma ** 2))
-posteriorSyntax = -( (T.sum(nSample * T.log(T.nnet.softmax(x))) + T.sum(T.log(normalPdfSyntax))) )
-posterior = theano.function(inputs=[nSample,x,u,sigma], outputs=posteriorSyntax)
+#normalPdfSyntax = 1. / T.sqrt(2 * 3.14159 * sigma ** 2) * T.exp(-(x-u) ** 2 / (2 * sigma ** 2))
+#posteriorSyntax = -( (T.sum(nSample * T.log(T.nnet.softmax(x))) + T.sum(T.log(normalPdfSyntax))) )
+posteriorSyntax = T.sum(nSample * T.log(T.nnet.softmax(x)))
+posterior = theano.function(inputs=[nSample,x], outputs=posteriorSyntax)
 #print posterior([2,2,1000],[1,2,3],0,5)
 
 gPosteriorSyntax = T.grad(cost=posteriorSyntax, wrt=x)
-gPosterior = theano.function(inputs=[nSample,x,u,sigma], outputs=gPosteriorSyntax)
+gPosterior = theano.function(inputs=[nSample,x], outputs=gPosteriorSyntax)
 #print gPosterior([2,2,1000],[1,2,3],0,5)
 
 
-estimated_parameter = hamiltonianMonteCarlo(numpy.random.normal(0., 1., 10),callPosterior,callGPosterior)
+estimated_parameter = hamiltonianMonteCarlo(numpy.random.normal(0., 1., 3),callPosterior,callGPosterior)
 
 print softmax(estimated_parameter)
 
